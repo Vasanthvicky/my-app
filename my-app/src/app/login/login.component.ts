acec14dc6e42bb1098fd1b860c4fa42836d87db5
Vigneshvasanth <vigneshvasanth.c@trustrace.com>
2021-06-17 00:03:42 +0530

    login page component created and few UI set up is created

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, NG_VALUE_ACCESSOR, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+const MULTI_SELECT_VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => LoginCustomizeFieldsMatcher),
+  multi: true
+};
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class LoginCustomizeFieldsMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -6,6 +22,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  matcher = new LoginCustomizeFieldsMatcher();
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
   constructor() { }
 
